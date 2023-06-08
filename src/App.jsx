e0669a69709e8ebc93c6ca1fb5c6cf3918cf99e5
MuneneCalvin <munenecalvn@gmail.com>
2023-06-08 10:38:08 +0300

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import NavBar from './components/NavBar';
+import News from './components/News';
+import { Routes, Route,} from "react-router-dom";
+import About from './components/About';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+      <navbar/>
+      <Routes>
+          <Route path="/"  element={<News pageSize={PageSize} country="in" category="general"/>} />
+          <Route path="/about"  element={<About/>} />
+
+          {/* category */}
+          <Route exact path="/general" element={<News key="general"  pageSize={PageSize} country="in" category="general"/>} />
+          <Route exact path="/business" element={<News key="business" pageSize={PageSize} country="in" category="business"/>} />
+          <Route exact path="/entertainment" element={<News   key="entertainment" pageSize={PageSize} country="in" category="entertainment"/>} />
+          <Route exact path="/health" element={<News key="health" pageSize={PageSize} country="in" category="health"/>} />
+          <Route exact path="/science"element={<News key="science" pageSize={PageSize} country="in" category="science"/>} />
+          <Route exact path="/sports" element={<News key="sports" pageSize={PageSize} country="in" category="sports"/>} />
+          <Route exact path="/technology" element={<News key="technology" pageSize={PageSize} country="in" category="technology"/>} />
+          
+
+      </Routes> 
+
+      </BrowserRouter>
     </>
   )
 }
